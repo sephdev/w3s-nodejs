fc469bea64dceaa3c4931bea8efd882d88d637cc
Joseph Tandingan <josephdevapps@gmail.com>
2018-07-31 00:48:16 +0800

    Uploading of File (image) Working

--- uploadfile.js
+++ uploadfile.js
@@ -1,17 +1,23 @@
 var http = require('http');
 var formidable = require('formidable');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
     if (req.url == '/fileupload') {
         var form = new formidable.IncomingForm();
         form.parse(req, function (err, fields, files) {
-            res.write('File uploaded');
-            res.end();
+            var oldpath = files.filetoupload.path;
+            var newpath = '/Users/josephtandingan/node/w3s/..' + files.filetoupload.name;
+            fs.rename(oldpath, newpath, function (err) {
+                if (err) throw err;
+                res.write('File uploaded and moved');
+                res.end();
+            });
         });
     } else {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-        res.write(',input type="file" name="filetoupload"><br>');
+        res.write('<input type="file" name="filetoupload"><br>');
         res.write('<input type="submit">');
         res.write('</form>');
         return res.end();
