9d51ca3380edf8294df90580933d0e711f4f3889
Joseph Tandingan <josephdevapps@gmail.com>
2018-07-30 23:22:02 +0800

    Fileserver and Parse Demo

---
+++ parse.js
@@ -0,0 +1,11 @@
+var url = require('url');
+var adr = 'http://localhost:8080/default.htm?year=2018&month=july';
+var q = url.parse(adr, true);
+
+console.log(q.host); // returns 'localhost:8080'
+console.log(q.pathname); // returns '/default.htm'
+console.log(q.search); // returns '?year=2018&month=july'
+
+var qdata = q.query; // returns an object: { year: 2018, month: 'july' }
+console.log(qdata.month); //returns 'july'
+console.log(qdata.year);
